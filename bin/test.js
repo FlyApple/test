72c4b74c0e96a2caee70a922b5486a809269466b
FlyApple <silly@Sillys-MacBook-Pro.local>
2017-12-29 03:03:43 +0800

    20171229-3

--- bin/test.js
+++ bin/test.js
@@ -1,5 +1,15 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const http = require("http");
 main();
 function main() {
-    console.log("hello world");
+    var http_server = http.createServer(onRequest);
+    http_server.listen(2200, "0.0.0.0");
+    console.log("Server listen port 2200.");
+}
+function onRequest(request, response) {
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.write("Hello world");
+    response.end();
 }
 //# sourceMappingURL=test.js.map
--- src/test.ts
+++ src/test.ts
@@ -1,10 +1,25 @@
-//import * as http from 'http';
-//import http = require('http');
+/// <reference path="../typings/globals/node/index.d.ts" />
 
+import http = require("http");
+import url = require("url");
+
+//
 main();
 
 function main()
 {
     //
-    console.log("hello world");
+    //console.log(process.mainModule.paths);
+
+    var http_server = http.createServer(onRequest);
+    http_server.listen(2200, "0.0.0.0");
+
+    console.log("Server listen port 2200.");
+}
+
+function onRequest(request, response)
+{
+    response.writeHead(200,{"Content-Type":"text/plain"});
+    response.write("Hello world");
+    response.end();
 }
